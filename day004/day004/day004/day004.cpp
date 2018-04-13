5494e76a67d0c5458542eb3003d8daac45f1c8db
Allenboy <32590722+haidragon@users.noreply.github.com>
2018-04-13 19:57:55 -0400

    Add files via upload

---
+++ day004/day004/day004/day004.cpp
@@ -0,0 +1,32 @@
+// day004.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+
+//if
+
+//if else
+
+//swicth
+
+
+int main()
+{
+	int a = 55, b = 66;
+	//if
+	if (a==b){
+		printf("a==b");
+	}
+	else if(a > b)
+	{
+		printf("a>b");
+	}
+	else
+	{
+		printf("a<b");
+	}
+
+    return 0;
+}
+
---
+++ day004/day004/循环结构/循环结构.cpp
@@ -0,0 +1,32 @@
+// 循环结构.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+//for
+
+//while
+
+//do while
+
+int main()
+{
+	//for
+// 	char buff[10] = { "hellowo" };
+// 	for (int i = 0; i < 10; i++)
+// 	{
+// 		printf("%d", buff[i]);
+// 	}
+// 	int i = 9;
+// 	while (i!=0){
+// 		i--;
+// 	}
+	int i = 9;
+	do 
+	{
+		printf("%d", i);
+	} while (i--);
+
+    return 0;
+}
+
